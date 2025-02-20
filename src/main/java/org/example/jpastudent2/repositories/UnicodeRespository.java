3ddd3935b3d3e2ac288ab0d0c526dc68e99aea7f
amaliekea <amje0001@stud.kea.dk>
2025-02-20 11:50:23 +0100

    nu med deploy yml

--- src/main/java/org/example/jpastudent2/repositories/UnicodeRespository.java
+++ src/main/java/org/example/jpastudent2/repositories/UnicodeRespository.java
@@ -2,6 +2,6 @@ package org.example.jpastudent2.repositories;
 
 import org.example.jpastudent2.model.Unicode;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//hej
 public interface UnicodeRespository extends JpaRepository<Unicode, Long> {
 }
